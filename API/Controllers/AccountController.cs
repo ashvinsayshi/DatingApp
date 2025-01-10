3fe671d252e461df6bc5c4211e4590a85225b1cf
ashvinsayshi <clickashvin@gmail.com>
2025-01-10 23:18:16 +0400

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -57,7 +57,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+            .Include(x => x.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if(user == null) return Unauthorized("invalid username");
 
@@ -73,7 +75,9 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+
             };
 
         }
--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,17 +1,5 @@
-import { inject } from '@angular/core';
-import { CanDeactivateFn } from '@angular/router';
-import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { CanActivateFn } from '@angular/router';
 
-export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (
-  component: MemberEditComponent): boolean => {
-  console.log(component.editForm?.dirty);
-  if (!component.editForm) {
-    return true;
-  }
-  if (component.editForm.dirty) {
-    console.log('dirty');
-    return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
-  }
-  console.log('not dirty');
+export const preventUnsavedChangesGuard: CanActivateFn = (route, state) => {
   return true;
 };
